ed13ae01944fe49e2c4956662e5cbe6bd729ae72
Pratibha21-git <prati21313@gmail.com>
2026-06-08 16:18:57 +0530

    Day5 JDBC Done

--- backend/sms/src/main/java/com/example/sms/controller/StudentController.java
+++ backend/sms/src/main/java/com/example/sms/controller/StudentController.java
@@ -1,15 +1,49 @@
 package com.example.sms.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import com.example.sms.model.Student;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/students")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins="http://localhost:5173")
 public class StudentController {
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @GetMapping
-    public Student getStudent() {
-        return new Student(1, "John Doe", "Computer Science");
+    public ArrayList<Student> getStudent() {
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(  new Student(1, "Ram", "MCA") );
+        students.add(  new Student(2, "Sakshi", "BE") );
+        students.add(  new Student(3, "Ritu", "BCA") );
+
+        return students;
     }
 
+    @GetMapping("/bca")
+    public List<Student> getBcaStudents(){
+        return  getStudent().stream()
+                  .filter (student-> "BCA".equals(student.getCourse()))
+                  .collect(Collectors.toList());
+
+    }
+
+    @GetMapping("/count")
+    public int countStudents(){
+        String sql = "SELECT COUNT(*) FROM students";
+        return jdbcTemplate.queryForObject(sql, Integer.class);
+    }
+  
 }
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,55 @@
-import { useState } from 'react';
-function App(){
-    const[students, setStudents] = useState(null);
-    const getStudents = async () => {
-            const response = await fetch('http://localhost:8080/students');
-            const data = await response.json();
-            setStudents(data);
-        
-    };
-    return (
-        <div>
+import { useState } from "react";
 
-            <button onClick = {getStudents}>
-                Fetch Student
-            </button>
-            <h1>Student Information</h1>
-            <h1>{students?.name}</h1>
-            <h1>{students?.id}</h1>
-            <h1>{students?.course}</h1>
-        </div>
-    );
+function App(){
+  const [students, setStudents] = useState([]);
+  const [count, setCount] = useState(0);
+
+  //function to get Student data from Backend
+  const getStudents = async ()=>{
+    const response = await fetch('http://localhost:8080/students');
+    const data = await response.json();
+    console.log(data);
+    setStudents(data);
+  }
+
+    const getBcaStudents = async ()=>{
+    const response = await fetch('http://localhost:8080/students/bca');
+    const data = await response.json();
+    console.log(data);
+    setStudents(data);
+  }
+  const fetchTotalStudentsCount = async ()=>{
+    const response = await fetch('http://localhost:8080/students/count');
+    const data = await response.json();
+    console.log(data);
+    setCount(data);
+  };
+
+  return(
+    <div>
+      <button onClick={getStudents}>
+        Fetch Students
+      </button>
+
+      <button onClick={getBcaStudents}>
+        Show BCA Students
+      </button>
+      
+      <ul>
+       {students.map(student=>(
+        <li key={student.id}>
+          {student.name} -  {student.course}
+        </li>
+       ))
+       }
+      </ul>
+
+      <button onClick = {fetchTotalStudentsCount}>
+        Total Students
+      </button>
+      <p>Total Students: {count}</p>
+
+    </div>
+  )
 }
 export default App;
