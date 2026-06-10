f4a31f6fe9a5b3e8b3bf7a0ee9435a6777a0fdfa
Pratibha21-git <prati21313@gmail.com>
2026-06-10 15:40:53 +0530

    Day 7 Spring Data JPA and Hibernate Done

--- backend/sms/src/main/java/com/example/sms/controller/StudentController.java
+++ backend/sms/src/main/java/com/example/sms/controller/StudentController.java
@@ -7,6 +7,8 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,23 +25,23 @@ public class StudentController {
     @Autowired
     private StudentService service;
 
-    @GetMapping
-    public ArrayList<Student> getStudent() {
-        ArrayList<Student> students = new ArrayList<>();
-        students.add(  new Student(1, "Ram", "MCA") );
-        students.add(  new Student(2, "Sakshi", "BE") );
-        students.add(  new Student(3, "Ritu", "BCA") );
+    // @GetMapping
+    // public ArrayList<Student> getStudent() {
+    //     ArrayList<Student> students = new ArrayList<>();
+    //     students.add(  new Student(1, "Ram", "MCA") );
+    //     students.add(  new Student(2, "Sakshi", "BE") );
+    //     students.add(  new Student(3, "Ritu", "BCA") );
 
-        return students;
-    }
+    //     return students;
+    // }
 
-    @GetMapping("/bca")
-    public List<Student> getBcaStudents(){
-        return  getStudent().stream()
-                  .filter (student-> "BCA".equals(student.getCourse()))
-                  .collect(Collectors.toList());
+    // @GetMapping("/bca")
+    // public List<Student> getBcaStudents(){
+    //     return  getStudent().stream()
+    //               .filter (student-> "BCA".equals(student.getCourse()))
+    //               .collect(Collectors.toList());
 
-    }
+    // }
 
     @GetMapping("/count")
     public int countStudents(){
@@ -49,6 +51,16 @@ public class StudentController {
     @GetMapping("/message")
     public String getMessage(){
         return service.getStudentInfo();
+    }
+
+    @GetMapping
+    public List<Student> getAllStudents(){
+        return service.getAllStudents();
+    }
+
+    @PostMapping
+    public Student addStudent(@RequestBody Student student){
+        return service.saveStudent(student);
     }
   
 }
--- backend/sms/src/main/java/com/example/sms/service/StudentService.java
+++ backend/sms/src/main/java/com/example/sms/service/StudentService.java
@@ -1,10 +1,18 @@
 package com.example.sms.service;
+
 import org.springframework.stereotype.Service;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import com.example.sms.model.Student;
+import com.example.sms.repository.StudentRepository;
+import java.util.List;
 
 @Service
 public class StudentService {
+
+    @Autowired
+    private StudentRepository repository;
+
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
@@ -15,5 +23,12 @@ public class StudentService {
     public Integer getStudentCount(){
         String sql = "SELECT COUNT(*) FROM students";
         return jdbcTemplate.queryForObject(sql, Integer.class);
+    }
+
+    public List<Student> getAllStudents(){
+        return repository.findAll();
+    }
+    public Student saveStudent(Student student){
+        return repository.save(student);
     }
 }
